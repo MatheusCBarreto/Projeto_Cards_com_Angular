be81d081462654fdad4ef3ac251fc18bc1ff1e0d
MatheusCBarreto <devmattbarreto@protonmail.com>
2024-08-02 12:12:33 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'Projeto_Cards'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Projeto_Cards');
+   
   });
 
   it('should render title', () => {
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input({ required: true }) btnText: string = "";
+  @Input() btnStyle: "white" | "purple" = "white";
+
+  @Input() isDisabled: boolean = false;
+
+  @Output() btnClickedEmit = new EventEmitter<void>();
+
+  onBtnClicked() {
+    this.btnClickedEmit.emit();
+  }
+}
